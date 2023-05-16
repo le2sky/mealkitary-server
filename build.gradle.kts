8d8cee0401cfe195dc9df857f251df427bbfe53a
leesky <leehaneul0623@gmail.com>
2023-05-17 08:57:14 +0900

    style : 코드 정렬

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,60 +2,60 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-	id("org.springframework.boot") version "2.7.11" apply false
-	id("io.spring.dependency-management") version "1.0.15.RELEASE" apply false
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21" apply false
-	kotlin("plugin.jpa") version "1.6.21" apply false
+    id("org.springframework.boot") version "2.7.11" apply false
+    id("io.spring.dependency-management") version "1.0.15.RELEASE" apply false
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21" apply false
+    kotlin("plugin.jpa") version "1.6.21" apply false
 }
 
 allprojects {
-	group = "com.mealkitary"
-	version = "0.0.1-SNAPSHOT"
+    group = "com.mealkitary"
+    version = "0.0.1-SNAPSHOT"
 
-	tasks {
-		withType<JavaCompile> {
-			sourceCompatibility = "11"
-			targetCompatibility = "11"
-		}
+    tasks {
+        withType<JavaCompile> {
+            sourceCompatibility = "11"
+            targetCompatibility = "11"
+        }
 
-		withType<KotlinCompile> {
-			kotlinOptions {
-				freeCompilerArgs = listOf("-Xjsr305=strict")
-				jvmTarget = "11"
-			}
-		}
+        withType<KotlinCompile> {
+            kotlinOptions {
+                freeCompilerArgs = listOf("-Xjsr305=strict")
+                jvmTarget = "11"
+            }
+        }
 
-		withType<BootJar> {
-			enabled = false
-		}
+        withType<BootJar> {
+            enabled = false
+        }
 
-		withType<Jar> {
-			enabled = true
-		}
+        withType<Jar> {
+            enabled = true
+        }
 
-		withType<Test> {
-			useJUnitPlatform()
-		}
-	}
+        withType<Test> {
+            useJUnitPlatform()
+        }
+    }
 
-	repositories {
-		mavenCentral()
-	}
+    repositories {
+        mavenCentral()
+    }
 }
 
 subprojects {
-	apply(plugin = "org.jetbrains.kotlin.jvm")
-	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
-	apply(plugin = "org.springframework.boot")
-	apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
 
-	dependencies {
-		implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-		implementation("org.jetbrains.kotlin:kotlin-reflect")
-		testImplementation("org.springframework.boot:spring-boot-starter-test")
-		testImplementation("io.kotest:kotest-runner-junit5:5.4.2")
-		testImplementation("io.kotest:kotest-assertions-core:5.4.2")
-	}
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("io.kotest:kotest-runner-junit5:5.4.2")
+        testImplementation("io.kotest:kotest-assertions-core:5.4.2")
+    }
 }
