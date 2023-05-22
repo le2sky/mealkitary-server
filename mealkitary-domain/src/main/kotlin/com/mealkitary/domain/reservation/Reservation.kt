53e819445b16e58e4fcd140daf83796455161544
leesky <leehaneul0623@gmail.com>
2023-05-23 05:04:54 +0900

    feat : reservation 생성 시 유효성 검사 기능 구현

--- mealkitary-domain/src/main/kotlin/com/mealkitary/domain/reservation/Reservation.kt
+++ mealkitary-domain/src/main/kotlin/com/mealkitary/domain/reservation/Reservation.kt
@@ -9,7 +9,21 @@ class Reservation private constructor(
 ) {
     companion object {
         fun of(lineItems: List<ReservationLineItem>, shop: Shop, reserveAt: LocalDateTime): Reservation {
+            checkLineItemsAtLeastOne(lineItems)
+            checkBeforeTime(reserveAt)
             return Reservation(lineItems, shop, reserveAt)
+        }
+
+        private fun checkLineItemsAtLeastOne(lineItems: List<ReservationLineItem>) {
+            if (lineItems.isEmpty()) {
+                throw IllegalArgumentException("예약 상품은 적어도 한 개 이상이어야 합니다.")
+            }
+        }
+
+        private fun checkBeforeTime(reserveAt: LocalDateTime) {
+            if (reserveAt.isBefore(LocalDateTime.now())) {
+                throw IllegalStateException("예약 시간은 적어도 미래 시점이어야 합니다.")
+            }
         }
     }
 }
--- mealkitary-domain/src/test/kotlin/com/mealkitary/domain/ReservationTest.kt
+++ mealkitary-domain/src/test/kotlin/com/mealkitary/domain/ReservationTest.kt
@@ -3,7 +3,9 @@ package com.mealkitary.domain
 import com.mealkitary.domain.reservation.Reservation
 import com.mealkitary.domain.reservation.ReservationLineItem
 import com.mealkitary.domain.reservation.Shop
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.throwable.shouldHaveMessage
 import java.time.LocalDateTime
 
 internal class ReservationTest : AnnotationSpec() {
@@ -11,5 +13,25 @@ internal class ReservationTest : AnnotationSpec() {
     @Test
     fun `예약 생성 테스트`() {
         Reservation.of(listOf(ReservationLineItem()), Shop(), LocalDateTime.now())
+    }
+
+    @Test
+    fun `예약 상품이 하나도 존재하지 않는다면 예외를 발생한다`() {
+        shouldThrow<IllegalArgumentException> {
+            Reservation.of(emptyList(), Shop(), LocalDateTime.now())
+        } shouldHaveMessage "예약 상품은 적어도 한 개 이상이어야 합니다."
+    }
+
+    @Test
+    fun `예약 시간이 현재 시간보다 과거라면 예외를 발생한다`() {
+        shouldThrowWhenAcceptBeforeReservationTime(LocalDateTime.now().minusDays(1))
+        shouldThrowWhenAcceptBeforeReservationTime(LocalDateTime.now().minusHours(1))
+        shouldThrowWhenAcceptBeforeReservationTime(LocalDateTime.now().minusYears(1))
+    }
+
+    private fun shouldThrowWhenAcceptBeforeReservationTime(beforeOneDays: LocalDateTime) {
+        shouldThrow<IllegalStateException> {
+            Reservation.of(listOf(ReservationLineItem()), Shop(), beforeOneDays)
+        } shouldHaveMessage "예약 시간은 적어도 미래 시점이어야 합니다."
     }
 }
