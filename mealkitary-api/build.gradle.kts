2481b1b9e386c9546025e32b4e6c54a7902cac18
leesky <leehaneul0623@gmail.com>
2023-05-25 03:59:40 +0900

    chore : jacoco 설정 및 gradle dsl 정리

--- mealkitary-api/build.gradle.kts
+++ mealkitary-api/build.gradle.kts
@@ -1,6 +1,7 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 dependencies {
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation(project(":mealkitary-application"))
     implementation(project(":mealkitary-domain"))
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,27 @@
 rootProject.name = "mealkitary"
+
 include(
     "mealkitary-api",
     "mealkitary-application",
     "mealkitary-domain",
     "mealkitary-infrastructure:adapter-persistence-spring-data-jpa"
 )
+
+pluginManagement {
+    val kotlinVersion: String by settings
+    val ktlintVersion: String by settings
+    val springBootVersion: String by settings
+    val springDependencyManagementVersion: String by settings
+
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "org.springframework.boot" -> useVersion(springBootVersion)
+                "io.spring.dependency-management" -> useVersion(springDependencyManagementVersion)
+                "org.jetbrains.kotlin.jvm", "org.jetbrains.kotlin.plugin.spring",
+                "org.jetbrains.kotlin.plugin.jpa" -> useVersion(kotlinVersion)
+                "org.jlleitschuh.gradle.ktlint" -> useVersion(ktlintVersion)
+            }
+        }
+    }
+}
