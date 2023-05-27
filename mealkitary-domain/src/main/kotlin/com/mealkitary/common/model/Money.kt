6ab2935c8b8d00f9bd4de14b104facd10b071286
leesky <leehaneul0623@gmail.com>
2023-05-28 03:55:24 +0900

    feat : money 모델 구현

--- mealkitary-domain/src/main/kotlin/com/mealkitary/common/model/Money.kt
+++ mealkitary-domain/src/main/kotlin/com/mealkitary/common/model/Money.kt
@@ -1,3 +1,26 @@
 package com.mealkitary.common.model
 
-class Money(private val value: Int)
+data class Money private constructor(val value: Int) {
+
+    operator fun times(target: Int): Money {
+        return of(value * target)
+    }
+
+    operator fun plus(target: Money): Money {
+        return of(value + target.value)
+    }
+
+    companion object {
+        fun of(value: Int): Money {
+            checkValue(value)
+            return Money(value)
+        }
+
+        private fun checkValue(value: Int) {
+            if (value < 0) {
+                throw IllegalArgumentException("돈은 음수가 될 수 없습니다.")
+            }
+        }
+    }
+}
+
