55335dc115a9ba1d4b54ecfae444b4b4038c2869
leesky <leehaneul0623@gmail.com>
2023-05-31 07:46:44 +0900

    feat : shop 영속성 어댑터 구현

--- mealkitary-infrastructure/adapter-persistence-spring-data-jpa/src/main/resources/data.sql
+++ mealkitary-infrastructure/adapter-persistence-spring-data-jpa/src/main/resources/data.sql
@@ -24,6 +24,10 @@ insert into product
 values (5, '토마토 훠궈', 22000, 2);
 insert into product
 values (6, '닭가슴살 샐러드', 6600, 2);
+insert into reservable_time
+values (2, '12:30');
+insert into reservable_time
+values (2, '19:30');
 
 insert into shop
 values (3, 'VALID', '집밥뚝딱 숭실대입구점');
@@ -33,3 +37,11 @@ insert into product
 values (8, '토마토 파스타', 8800, 3);
 insert into product
 values (9, '알리오올리오 파스타', 9900, 3);
+insert into reservable_time
+values (3, '16:30');
+insert into reservable_time
+values (3, '18:30');
+insert into reservable_time
+values (3, '20:00');
+insert into reservable_time
+values (3, '23:30');
