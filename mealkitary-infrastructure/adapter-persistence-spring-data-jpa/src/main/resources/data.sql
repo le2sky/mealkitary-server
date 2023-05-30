592a9b0153c629e34b553b0c062f201e17638a0f
leesky <leehaneul0623@gmail.com>
2023-05-31 00:40:06 +0900

    feat : 임베디드 DB 사용시 데이터 초기화하도록 설정

---
+++ mealkitary-infrastructure/adapter-persistence-spring-data-jpa/src/main/resources/data.sql
@@ -0,0 +1,35 @@
+insert into shop
+values (1, 'VALID', '집밥뚝딱 철산점');
+insert into product
+values (1, '부대찌개', 15800, 1);
+insert into product
+values (2, '존슨탕', 19600, 1);
+insert into product
+values (3, '소불고기 샤브샤브', 13200, 1);
+insert into reservable_time
+values (1, '06:30');
+insert into reservable_time
+values (1, '09:30');
+insert into reservable_time
+values (1, '12:30');
+insert into reservable_time
+values (1, '18:30');
+
+
+insert into shop
+values (2, 'VALID', '집밥뚝딱 안양점');
+insert into product
+values (4, '비비고 만두', 3200, 2);
+insert into product
+values (5, '토마토 훠궈', 22000, 2);
+insert into product
+values (6, '닭가슴살 샐러드', 6600, 2);
+
+insert into shop
+values (3, 'VALID', '집밥뚝딱 숭실대입구점');
+insert into product
+values (7, '왕만두', 4900, 3);
+insert into product
+values (8, '토마토 파스타', 8800, 3);
+insert into product
+values (9, '알리오올리오 파스타', 9900, 3);
