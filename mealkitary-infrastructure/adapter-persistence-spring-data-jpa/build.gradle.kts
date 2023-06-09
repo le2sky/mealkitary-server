1b6ddcc6ca65d3a16911d1d5eefbeef7229264ae
leesky <leehaneul0623@gmail.com>
2023-06-09 23:31:53 +0900

    chore : querydsl, kapt 의존성 설정

--- mealkitary-infrastructure/adapter-persistence-spring-data-jpa/build.gradle.kts
+++ mealkitary-infrastructure/adapter-persistence-spring-data-jpa/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies {
+    val querydslVersion: String by properties
+    implementation("com.querydsl:querydsl-jpa:$querydslVersion")
     implementation("com.h2database:h2")
     implementation(project(":mealkitary-domain"))
     implementation(project(":mealkitary-application"))
