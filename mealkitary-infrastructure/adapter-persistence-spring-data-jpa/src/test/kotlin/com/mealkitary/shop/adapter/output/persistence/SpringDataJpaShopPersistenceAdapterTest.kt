2bcf2abc453d4a75e0d201a6ba883a41f3e8bf3d
le2sky <leehaneul990623@gmail.com>
2023-08-02 08:56:09 +0900

    feat : 프로덕트가 함께 로드된 가게 조회 기능 구현

--- mealkitary-infrastructure/adapter-persistence-spring-data-jpa/src/test/kotlin/com/mealkitary/shop/adapter/output/persistence/SpringDataJpaShopPersistenceAdapterTest.kt
+++ mealkitary-infrastructure/adapter-persistence-spring-data-jpa/src/test/kotlin/com/mealkitary/shop/adapter/output/persistence/SpringDataJpaShopPersistenceAdapterTest.kt
@@ -3,15 +3,18 @@ package com.mealkitary.shop.adapter.output.persistence
 import com.mealkitary.common.model.Money
 import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.extensions.spring.SpringExtension
+import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.context.annotation.Import
 import java.time.LocalTime
+import javax.persistence.EntityManagerFactory
 
 @DataJpaTest
 @Import(SpringDataJpaShopPersistenceAdapter::class)
 class SpringDataJpaShopPersistenceAdapterTest(
-    private val adapterUnderTest: SpringDataJpaShopPersistenceAdapter
+    private val adapterUnderTest: SpringDataJpaShopPersistenceAdapter,
+    private val emf: EntityManagerFactory
 ) : AnnotationSpec() {
 
     override fun extensions() = listOf(SpringExtension)
@@ -31,6 +34,16 @@ class SpringDataJpaShopPersistenceAdapterTest(
         products.size shouldBe 3
         products.get(0).name shouldBe "부대찌개"
         products.get(0).price shouldBe Money.from(15800)
+    }
+
+    @Test
+    fun `db integration test - 가게 ID에 해당하는 가게를 조회하는데, 프로덕트도 함께 로드된다`() {
+        val shop = adapterUnderTest.loadOneShopById(1L)
+
+        emf.persistenceUnitUtil.isLoaded(shop.products).shouldBeTrue()
+        shop.title shouldBe "집밥뚝딱 철산점"
+        shop.products.size shouldBe 3
+        shop.reservableTimes.size shouldBe 4
     }
 
     @Test
