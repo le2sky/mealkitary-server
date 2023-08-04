221e1fa5266a9d8b9ca41a60539d94ac0d917beb
le2sky <leehaneul990623@gmail.com>
2023-08-05 04:06:19 +0900

    feat : 존재하지 않는 가게 조회시 예외 발생 기능 추가

--- mealkitary-infrastructure/adapter-persistence-spring-data-jpa/src/test/kotlin/com/mealkitary/shop/adapter/output/persistence/SpringDataJpaShopPersistenceAdapterTest.kt
+++ mealkitary-infrastructure/adapter-persistence-spring-data-jpa/src/test/kotlin/com/mealkitary/shop/adapter/output/persistence/SpringDataJpaShopPersistenceAdapterTest.kt
@@ -1,9 +1,12 @@
 package com.mealkitary.shop.adapter.output.persistence
 
 import com.mealkitary.PersistenceIntegrationTestSupport
+import com.mealkitary.common.exception.EntityNotFoundException
 import com.mealkitary.common.model.Money
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.shouldHaveMessage
 import java.time.LocalTime
 import javax.persistence.EntityManagerFactory
 
@@ -30,6 +33,13 @@ class SpringDataJpaShopPersistenceAdapterTest(
     }
 
     @Test
+    fun `db integration test - 가게 ID에 해당하는 가게의 상품 목록을 조회할 때, 해당 가게가 존재하지 않는 경우 예외를 발생한다`() {
+        shouldThrow<EntityNotFoundException> {
+            adapterUnderTest.loadAllProductByShopId(99L)
+        } shouldHaveMessage "존재하지 않는 가게입니다."
+    }
+
+    @Test
     fun `db integration test - 가게 ID에 해당하는 가게를 조회하는데, 프로덕트도 함께 로드된다`() {
         val shop = adapterUnderTest.loadOneShopById(1L)
 
@@ -40,10 +50,24 @@ class SpringDataJpaShopPersistenceAdapterTest(
     }
 
     @Test
+    fun `db integration test - 가게 ID에 해당하는 가게를 조회할 때, 해당 가게가 존재하지 않는 경우 예외를 발생한다`() {
+        shouldThrow<EntityNotFoundException> {
+            adapterUnderTest.loadOneShopById(99L)
+        } shouldHaveMessage "존재하지 않는 가게입니다."
+    }
+
+    @Test
     fun `db integration test - 가게 ID에 해당하는 가게의 모든 예약 가능 시간을 조회한다`() {
         val reservableTimes = adapterUnderTest.loadAllReservableTimeByShopId(1L)
 
         reservableTimes.size shouldBe 4
         reservableTimes.get(0) shouldBe LocalTime.of(6, 30)
+    }
+
+    @Test
+    fun `db integration test - 가게 ID에 해당하는 가게의 모든 예약 가능 시간을 조회할 때, 해당 가게가 존재하지 않는 경우 예외를 발생한다`() {
+        shouldThrow<EntityNotFoundException> {
+            adapterUnderTest.loadAllReservableTimeByShopId(99L)
+        } shouldHaveMessage "존재하지 않는 가게입니다."
     }
 }
