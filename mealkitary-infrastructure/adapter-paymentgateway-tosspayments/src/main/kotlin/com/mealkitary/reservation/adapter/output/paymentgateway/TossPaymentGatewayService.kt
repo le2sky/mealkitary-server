02657ae6f9665efb65765e0f474ac10b5f886f7c
le2sky <leehaneul990623@gmail.com>
2023-08-09 04:10:39 +0900

    feat : toss pg 신규 모듈 생성

---
+++ mealkitary-infrastructure/adapter-paymentgateway-tosspayments/src/main/kotlin/com/mealkitary/reservation/adapter/output/paymentgateway/TossPaymentGatewayService.kt
@@ -0,0 +1,11 @@
+package com.mealkitary.reservation.adapter.output.paymentgateway
+
+import com.mealkitary.reservation.domain.payment.Payment
+import com.mealkitary.reservation.domain.payment.PaymentGatewayService
+
+class TossPaymentGatewayService : PaymentGatewayService {
+
+    override fun confirm(payment: Payment) {
+        TODO("Not yet implemented")
+    }
+}
