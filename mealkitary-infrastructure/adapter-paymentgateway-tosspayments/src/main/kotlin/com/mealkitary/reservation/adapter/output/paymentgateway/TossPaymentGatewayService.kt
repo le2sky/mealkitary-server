e35568675e27c5839e94bbdb32bd8ee663e864bd
le2sky <leehaneul990623@gmail.com>
2023-08-12 00:22:56 +0900

    feat : Toss PG 서비스 구현

--- mealkitary-infrastructure/adapter-paymentgateway-tosspayments/src/main/kotlin/com/mealkitary/reservation/adapter/output/paymentgateway/TossPaymentGatewayService.kt
+++ mealkitary-infrastructure/adapter-paymentgateway-tosspayments/src/main/kotlin/com/mealkitary/reservation/adapter/output/paymentgateway/TossPaymentGatewayService.kt
@@ -2,10 +2,20 @@ package com.mealkitary.reservation.adapter.output.paymentgateway
 
 import com.mealkitary.reservation.domain.payment.Payment
 import com.mealkitary.reservation.domain.payment.PaymentGatewayService
+import org.springframework.stereotype.Component
 
-class TossPaymentGatewayService : PaymentGatewayService {
+@Component
+class TossPaymentGatewayService(
+    private val tossPaymentWebClient: TossPaymentWebClient
+) : PaymentGatewayService {
 
     override fun confirm(payment: Payment) {
-        TODO("Not yet implemented")
+        val tossPayment = TossPayment.of(
+            paymentKey = payment.paymentKey,
+            orderId = payment.reservation.id.toString(),
+            amount = payment.amount.value
+        )
+
+        tossPaymentWebClient.requestConfirm(tossPayment, "https://api.tosspayments.com")
     }
 }
