0e566c21908fb47e56a88e80f196c2269a43a9f7
le2sky <leehaneul990623@gmail.com>
2023-09-20 02:54:48 +0900

    feat : 가게 식별자 기반 예약 조회 API 구현

--- mealkitary-api/src/main/kotlin/com/mealkitary/reservation/web/GetReservationController.kt
+++ mealkitary-api/src/main/kotlin/com/mealkitary/reservation/web/GetReservationController.kt
@@ -1,10 +1,14 @@
 package com.mealkitary.reservation.web
 
+import com.mealkitary.common.utils.HttpResponseUtils
 import com.mealkitary.common.utils.UUIDUtils
 import com.mealkitary.reservation.application.port.input.GetReservationQuery
+import com.mealkitary.reservation.application.port.input.ReservationResponse
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -16,4 +20,12 @@ class GetReservationController(
     @GetMapping("/{reservationId}")
     fun getOneReservation(@PathVariable("reservationId") reservationId: String) =
         getReservationQuery.loadOneReservationById(UUIDUtils.fromString(reservationId))
+
+    @GetMapping
+    fun getAllReservation(@RequestParam("shopId") shopIdParam: Long?): ResponseEntity<List<ReservationResponse>> {
+        val shopId = shopIdParam ?: throw IllegalArgumentException("가게 식별자는 필수입니다.")
+
+        return HttpResponseUtils
+            .mapToResponseEntity(emptiableList = getReservationQuery.loadAllReservationByShopId(shopId))
+    }
 }
