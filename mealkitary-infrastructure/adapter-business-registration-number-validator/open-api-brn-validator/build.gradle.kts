6984274c2f9a95bc639588052f9ee358e403aed4
le2sky <leehaneul990623@gmail.com>
2023-10-03 03:00:05 +0900

    chore : webflux 의존성 설정

--- mealkitary-infrastructure/adapter-business-registration-number-validator/open-api-brn-validator/build.gradle.kts
+++ mealkitary-infrastructure/adapter-business-registration-number-validator/open-api-brn-validator/build.gradle.kts
@@ -1,3 +1,7 @@
 dependencies {
+    val mockWebServerVersion: String by properties
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation(project(":mealkitary-domain"))
+    testImplementation("com.squareup.okhttp3:mockwebserver:$mockWebServerVersion")
+    testImplementation("io.projectreactor:reactor-test")
 }
