534af9cf03455e7f3582c78946b00ab7ff79fe7c
박은지 <qkrdmswl1018@naver.com>
2023-10-03 15:04:34 +0900

    chore: 주소 변환기 모듈 의존성 추가

---
+++ mealkitary-infrastructure/adapter-address-resolver/build.gradle.kts
@@ -0,0 +1,8 @@
+dependencies {
+    val querydslVersion: String by properties
+    implementation("com.querydsl:querydsl-jpa:$querydslVersion")
+    implementation("com.h2database:h2")
+    implementation(project(":mealkitary-domain"))
+    implementation(project(":mealkitary-application"))
+    testImplementation(testFixtures(project(":mealkitary-domain")))
+}
