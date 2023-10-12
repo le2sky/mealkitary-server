f5cc54db0e5f8d93239f2966f5fa03a06d8cf2aa
le2sky <leehaneul990623@gmail.com>
2023-10-13 05:25:07 +0900

    fix : 불필요한 주석 제거

--- mealkitary-infrastructure/adapter-address-resolver/kakao-api-address-resolver/src/main/kotlin/com/mealkitary/address/payload/KakaoApiAddressResponse.kt
+++ mealkitary-infrastructure/adapter-address-resolver/kakao-api-address-resolver/src/main/kotlin/com/mealkitary/address/payload/KakaoApiAddressResponse.kt
@@ -10,6 +10,7 @@ data class Meta(
     val pageable_count: Int,
     val total_count: Int
 )
+
 data class Document(
     val address_name: String,
     val address_type: String,
@@ -49,52 +50,3 @@ class RoadAddress(
     val y: String,
     val zone_no: String
 )
-
-/**
- *{
- *     "documents": [
- *         {
- *             "address": {
- *                 "address_name": "경기 안양시 만안구 안양동 756-15",
- *                 "b_code": "4117110100",
- *                 "h_code": "4117158100",
- *                 "main_address_no": "756",
- *                 "mountain_yn": "N",
- *                 "region_1depth_name": "경기",
- *                 "region_2depth_name": "안양시 만안구",
- *                 "region_3depth_h_name": "안양9동",
- *                 "region_3depth_name": "안양동",
- *                 "sub_address_no": "15",
- *                 "x": "126.909658990799",
- *                 "y": "37.3921505309817"
- *             },
- *             "address_name": "경기 안양시 만안구 병목안로 129",
- *             "address_type": "ROAD_ADDR",
- *             "road_address": {
- *                 "address_name": "경기 안양시 만안구 병목안로 129",
- *                 "building_name": "",
- *                 "main_building_no": "129",
- *                 "region_1depth_name": "경기",
- *                 "region_2depth_name": "안양시 만안구",
- *                 "region_3depth_name": "안양동",
- *                 "road_name": "병목안로",
- *                 "sub_building_no": "",
- *                 "underground_yn": "N",
- *                 "x": "126.909658990799",
- *                 "y": "37.3921505309817",
- *                 "zone_no": "14023"
- *             },
- *             "x": "126.909658990799",
- *             "y": "37.3921505309817"
- *         }
- *     ],
- *     "meta": {
- *         "is_end": true,
- *         "pageable_count": 1,
- *         "total_count": 1
- *     }
- * }
- *
- *
- *
- */
